fe4c1c123b40d0c2293954e9670a50f734404b7f
Mateo Schauvinhold <Mateo_schauvinhold@hotmail.com>
2026-09-22 23:20:52 -0300

    cambios en contacto

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -46,7 +46,7 @@ export const Contact = () => {
                 <svg className="w-5 h-5 text-primary-400 mt-1" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"/>
                 </svg>
-                <span className="text-gray-300">Dirección: Gral. Madariaga 1111, Paso de los Libres, Corrientes, Argentina</span>
+                <span className="text-gray-300">Ubicacion: Paso de los Libres, Corrientes, Argentina</span>
               </div>
               
               <div className="flex items-start space-x-3">
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   businessName: "The BullDog Growshop",
-  whatsappNumber: "+543772406996", // Formato internacional
+  whatsappNumber: "+'541123456789'", // Formato internacional
   instagramLink: "https://www.instagram.com/thebullgrowshop/",
   baseWhatsappMessage: "Hola, quiero realizar el siguiente pedido:",
   freeShippingMinAmount: 150000, // Monto mínimo para envíos gratis
